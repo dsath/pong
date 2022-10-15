64ffb28a6095aed2a6e9958417307703f5e99b31
Devin Sather <devin.sather@outlook.com>
2022-10-15 08:11:47 -0700

    Refactor

--- pong.js
+++ pong.js
@@ -27,81 +27,81 @@ class Pong {
 
   start() {
     this.app.ticker.add((delta) => {
-      this.leftPaddle.updatePhysicals();
-      this.ball.updatePhysicals();
+      this._updateContactPoints();
 
       this.ball.x += this.ball.horizontalMovement;
       this.ball.y += this.ball.verticalMovement;
-      let quadrant = this._ballHitsLeftPaddle();
-      console.log(quadrant);
-      switch (quadrant) {
-        case 2:
-          this.ball.changeMovement(2, -3);
-          break;
+      if (this._ballHitsLeftPaddle()) {
+        this._leftPaddleHit();
+      }
 
-        case 1:
-          this.ball.changeMovement(2, -1);
-          break;
-
-        case -1:
-          this.ball.changeMovement(2, 1);
-          break;
-
-        case -2:
-          this.ball.changeMovement(2, 3);
-          break;
-
-        default:
-          break;
+      if (this._ballHitsBarrier) {
       }
     });
   }
 
+  _ballHitsBarrier() {
+    if (this.ball.verticalContact) {
+    }
+  }
+  _updateContactPoints() {
+    this.leftPaddle.updateContactPoints();
+    this.ball.updateContactPoints();
+  }
+
+  // Returns true if ball is in contact with left paddle
   _ballHitsLeftPaddle() {
+    if (
+      this.ball.horizontalContact[0] - this.leftPaddle.horizontalContact <
+      0
+    ) {
+      if (
+        this.ball.y >= this.leftPaddle.verticalContact[1] &&
+        this.ball.y <= this.leftPaddle.verticalContact[0]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Redirects ball after hitting left paddle
+  _leftPaddleHit() {
     const fourthQuadrant = [
-      (this.leftPaddle.physicalY[1] + this.leftPaddle.y) / 2,
-      this.leftPaddle.physicalY[1],
+      (this.leftPaddle.verticalContact[1] + this.leftPaddle.y) / 2,
+      this.leftPaddle.verticalContact[1],
     ];
     const thirdQuadrant = [
       this.leftPaddle.y,
-      (this.leftPaddle.physicalY[1] + this.leftPaddle.y) / 2,
+      (this.leftPaddle.verticalContact[1] + this.leftPaddle.y) / 2,
     ];
     const secondQuadrant = [
-      (this.leftPaddle.physicalY[0] + this.leftPaddle.y) / 2,
+      (this.leftPaddle.verticalContact[0] + this.leftPaddle.y) / 2,
       this.leftPaddle.y,
     ];
     const firstQuadrant = [
-      this.leftPaddle.physicalY[0],
-      (this.leftPaddle.physicalY[0] + this.leftPaddle.y) / 2,
+      this.leftPaddle.verticalContact[0],
+      (this.leftPaddle.verticalContact[0] + this.leftPaddle.y) / 2,
     ];
-    console.log(this.ball.y, fourthQuadrant);
 
-    if (this.ball.physicalX - this.leftPaddle.physicalX < 0) {
-      if (
-        this.ball.y >= fourthQuadrant[1] &&
-        this.ball.y <= fourthQuadrant[0]
-      ) {
-        return 2;
-      } else if (
-        this.ball.y >= thirdQuadrant[1] &&
-        this.ball.y <= thirdQuadrant[0]
-      ) {
-        return 1;
-      } else if (
-        this.ball.y >= secondQuadrant[1] &&
-        this.ball.y <= secondQuadrant[0]
-      ) {
-        return -1;
-      } else if (
-        this.ball.y >= firstQuadrant[1] &&
-        this.ball.y <= firstQuadrant[0]
-      ) {
-        return -2;
-      } else {
-        return 0;
-      }
+    if (this.ball.y >= fourthQuadrant[1] && this.ball.y <= fourthQuadrant[0]) {
+      this.ball.changeMovement(2, -3);
+    } else if (
+      this.ball.y >= thirdQuadrant[1] &&
+      this.ball.y <= thirdQuadrant[0]
+    ) {
+      this.ball.changeMovement(2, -1);
+    } else if (
+      this.ball.y >= secondQuadrant[1] &&
+      this.ball.y <= secondQuadrant[0]
+    ) {
+      this.ball.changeMovement(2, 1);
+    } else if (
+      this.ball.y >= firstQuadrant[1] &&
+      this.ball.y <= firstQuadrant[0]
+    ) {
+      this.ball.changeMovement(2, 3);
     }
-    return 0;
   }
 
   _insertPaddleL(width, height) {
@@ -121,12 +121,12 @@ class Pong {
     this.leftPaddle.xDifferential = this.leftPaddle.width / 2;
 
     // actual phsyical point of contact for paddle when coming into contact with ball
-    this.leftPaddle.updatePhysicals = () => {
-      this.leftPaddle.physicalY = [
+    this.leftPaddle.updateContactPoints = () => {
+      this.leftPaddle.verticalContact = [
         this.leftPaddle.y + this.leftPaddle.yDifferential, // bottom of paddle
         this.leftPaddle.y - this.leftPaddle.yDifferential, // top of paddle
       ];
-      this.leftPaddle.physicalX =
+      this.leftPaddle.horizontalContact =
         this.leftPaddle.x + this.leftPaddle.xDifferential;
     };
   }
@@ -143,18 +143,28 @@ class Pong {
     // creating my own members
 
     // Need these values to make the paddle coming into contact with the pong more realistic
-    this.ball.xDifferential = this.ball.width / 2;
+    this.ball.radius = this.ball.width / 2;
 
     // Actual physical point of contact for ball to hit paddle
-    this.ball.updatePhysicals = () => {
-      this.ball.physicalX = this.ball.x - this.ball.xDifferential;
+    this.ball.updateContactPoints = () => {
+      this.ball.verticalContact = [
+        this.ball.y + this.ball.radius,
+        this.ball.y - this.ball.radius,
+      ];
+      this.ball.horizontalContact = [
+        this.ball.x - this.ball.radius,
+        this.ball.x + this.ball.radius,
+      ];
     };
 
     // current movement of ball
-    this.ball.horizontalMovement = -1;
+    this.ball.horizontalMovement = -5;
+    this.ball.horizontalContact = [];
     this.ball.verticalMovement = 0;
+    this.ball.horizontalContact = [];
 
     // creating my own methods
+    this.ball.horizontalContact = [];
     this.ball.changeMovement = (x, y) => {
       this.ball.horizontalMovement = x;
       this.ball.verticalMovement = y;
